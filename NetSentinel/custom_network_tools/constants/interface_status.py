f0598d6c94fc5bb0a5aba187dfde5b3b85de6b7b
silv-error <joefrancis.silv@gmail.com>
2025-08-25 13:41:13 +0800

    fixed small bug in hardcoded data

--- NetSentinel/custom_network_tools/constants/interface_status.py
+++ NetSentinel/custom_network_tools/constants/interface_status.py
@@ -1,52 +1,42 @@
 INTERFACE_BRIEF_DATA = [
     '''
-        Interface              IP-Address      OK? Method Status                Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up   down  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up   down 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down down 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up   up  
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up   up 
-        GigabitEthernet2/2     unassigned      YES unset  administratively down down 
-        Vlan1                  unassigned      YES unset  administratively down down
+        Interface              IP-Address      OK? Method Status                    Protocol 
+        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       down  
+        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       down 
+        GigabitEthernet0/2     unassigned      YES unset  administratively down     down 
+        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       up  
+        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       up 
     ''',
     '''
-        Interface              IP-Address      OK? Method Status                Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up   up  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up   down 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down down 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up   down  
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up   down 
-        GigabitEthernet2/2     unassigned      YES unset  administratively down down 
-        Vlan1                  unassigned      YES unset  administratively down up
+        Interface              IP-Address      OK? Method Status                    Protocol 
+        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       up  
+        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       down 
+        GigabitEthernet0/2     unassigned      YES unset  administratively down     down 
+        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       down  
+        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       down 
     ''',
     '''
-        Interface              IP-Address      OK? Method Status                Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up   down  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up   up 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down up 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up   up  
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up   up 
-        GigabitEthernet2/2     unassigned      YES unset  administratively down down 
-        Vlan1                  unassigned      YES unset  administratively down up
+        Interface              IP-Address      OK? Method Status                    Protocol 
+        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       down  
+        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       up 
+        GigabitEthernet0/2     unassigned      YES unset  administratively down     up 
+        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       up  
+        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       up 
     ''',
     '''
-        Interface              IP-Address      OK? Method Status                Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up   up  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up   up 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down up 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up   up  
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up   up 
-        GigabitEthernet2/2     unassigned      YES unset  administratively down down 
-        Vlan1                  unassigned      YES unset  administratively down up
+        Interface              IP-Address      OK? Method Status                    Protocol 
+        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       up  
+        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       up 
+        GigabitEthernet0/2     unassigned      YES unset  administratively down     up 
+        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       up  
+        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       up 
     ''',
     '''
-        Interface              IP-Address      OK? Method Status                Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up   down  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up   down 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down up 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up   down
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up   down
-        GigabitEthernet2/2     unassigned      YES unset  administratively down down 
-        Vlan1                  unassigned      YES unset  administratively down up
+        Interface              IP-Address      OK? Method Status                    Protocol 
+        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       down  
+        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       down 
+        GigabitEthernet0/2     unassigned      YES unset  administratively down     up 
+        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       down
+        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       down
     '''
 ]
