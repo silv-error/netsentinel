889121ac865e7fbc9362eeac2598d565b917a13a
silv-error <joefrancis.silv@gmail.com>
2025-09-10 13:31:34 +0800

    updated constant data

--- NetSentinel/custom_network_tools/constants/interface_status.py
+++ NetSentinel/custom_network_tools/constants/interface_status.py
@@ -31,12 +31,4 @@ INTERFACE_BRIEF_DATA = [
         GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       up  
         GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       up 
     ''',
-    '''
-        Interface              IP-Address      OK? Method Status                    Protocol 
-        GigabitEthernet0/0.10  192.168.1.1     YES unset  administratively up       down  
-        GigabitEthernet0/1     192.168.2.1     YES unset  administratively up       down 
-        GigabitEthernet0/2     unassigned      YES unset  administratively down     up 
-        GigabitEthernet1/0.20  192.168.3.3     YES unset  administratively up       down
-        GigabitEthernet1/1     192.168.4.2     YES unset  administratively up       down
-    '''
 ]
